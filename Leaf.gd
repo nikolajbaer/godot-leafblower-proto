d152071134f883f7816037e7888284a588ee40da
Nikolaj Baer <nikolaj.baer@gmail.com>
2021-12-03 12:49:20 -0800

    add gravel vs litter

---
+++ Leaf.gd
@@ -0,0 +1,11 @@
+extends RigidBody
+
+var C = 0.00005
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if not self.sleeping:
+		var v = self.linear_velocity.normalized()
+		var vm = self.linear_velocity.length_squared()
+		self.apply_central_impulse(v * -vm * C)
--- Litter.gd
+++ Litter.gd
@@ -4,15 +4,20 @@ extends Spatial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var LITTER_CNT = 100
+export var litter_count = 500
 var gravel_template = preload("res://Gravel.tscn")
-#var leaf_template = preload("res://Leaf.tscn")
+var leaf_template = preload("res://Leaf.tscn")
 
 
 func _ready():
-	for i in range(LITTER_CNT):
-		var l = gravel_template.instance()
-		l.scale *= .2 + rand_range(-.1,.1)
+	for i in range(litter_count):
+		var l
+		if randf() > 0.25:
+			l = gravel_template.instance()
+		else:
+			l = leaf_template.instance()
+			l.rotation = Vector3(0,randf(),0)
+		l.scale *= 1 + rand_range(-.2,.2)
 		l.translation = Vector3(rand_range(-50,50),1,rand_range(-50,50))
 		self.add_child(l)
 	
